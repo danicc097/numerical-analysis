d9248f0b3a8f6fcc33d7a3e49b867f9644131f62
Daniel Castro <danicc097@gmail.com>
2023-03-09 18:26:43 +0100

    more probability

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 Remove-Item -Recurse -Force -Path .venv
 python -m venv .venv
 .venv/Scripts/Activate.ps1
-pip install pip-tools
-pip-sync
+# pip install pip-tools
+pip install -r .\requirements.txt
